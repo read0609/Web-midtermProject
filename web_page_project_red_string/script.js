cedcabfab7c889ed36cefa9271acc68421cca0de
DESKTOP-22A0EPS\user <mdyu1000@gmail.com>
2017-04-23 23:51:50 +0800

    上傳我的網站企劃 PPT 跟footer 的icon 但是點擊有問題

---
+++ web_page_project_red_string/script.js
@@ -0,0 +1,7 @@
+$('.share').click(function(){
+  $('share_icons').addClass('share_icons_display');
+});
+
+$('.share').mouseleave(function(){
+  $('share_icons').removeClass('share_icons_display');
+});
