cbefa4b951708dc184f8dbb7c8743e6268f2aefc
DESKTOP-22A0EPS\user <mdyu1000@gmail.com>
2017-04-24 15:06:24 +0800

    微調js 新增container

--- web_page_project_red_string/script.js
+++ web_page_project_red_string/script.js
@@ -1,7 +1,15 @@
+var isClick = false;
 $('.share').click(function(){
-  $('share_icons').addClass('share_icons_display');
+    if (!isClick) {
+        $('.share_icons').addClass('share_icons_display');
+        isClick = true;
+    }else{
+        $('.share_icons').removeClass('share_icons_display');
+        isClick = false;
+    }
 });
 
-$('.share').mouseleave(function(){
+
+/*$('.share').mouseleave(function(){
   $('share_icons').removeClass('share_icons_display');
-});
+});*/
