c593130ae65dab21298e9c8a47ba070dccbc95d8
read0609 <uiouio123654@gmail.com>
2017-04-25 21:43:15 +0800

    新增ppt的png

--- 參賽須知/InforPart.js
+++ 參賽須知/InforPart.js
@@ -28,7 +28,7 @@ window.onload = function() {
     OnsiteTTouch = true;
   });
 
-  OnsiteText.addEventListener("mouseout", function() {
+  RegWin.addEventListener("mouseout", function() {
     if (OnsiteTTouch) {
       OnsiteReg.style.width = "200px";
       OnsiteReg.style.padding = "0 60px 0 60px";
@@ -36,9 +36,6 @@ window.onload = function() {
       OnsiteText.style.display = "none";
       OnsiteTTouch = false;
     }
-  });
-
-  NetText.addEventListener("mouseout", function() {
     if (NetTTouch) {
       OnsiteReg.style.display = "block";
       NetReg.style.width = "200px";
@@ -77,6 +74,10 @@ window.onload = function() {
   var scoText = document.getElementById("text_scoreIntro");
   var equipText = document.getElementById("text_equipmentIntro");
   var checkText = document.getElementById("text_checkIntro");
+  var otherIntro = document.getElementById("otherIntro");
+  var scoTTouch = false,
+    equipTTouch = false,
+    checkTTouch = false;
 
   scoIntr.addEventListener("mouseover", function() {
     scoIntr.style.width = "150px";
@@ -86,28 +87,40 @@ window.onload = function() {
     checkIntr.style.display = "none";
   });
 
-  scoIntr.addEventListener("mouseout", function() {
-    scoIntr.style.width = "300px";
-    scoIntr.style.borderRadius = "150px";
-    scoText.style.display = "none";
-    equipIntr.style.display = "flex";
-    checkIntr.style.display = "flex";
+  scoText.addEventListener("mouseover", function() {
+    scoTTouch = true;
+    res(scoTTouch, scoText, scoText, [equipIntr, checkText]);
   });
 
-  equipIntr.addEventListener("mouseover", function() {
+  scoText.addEventListener("mouseout", function() {
+    scoTTouch = false;
+    res(scoTTouch, scoText, scoText, [equipIntr, checkText]);
+  });
+
+  scoIntr.addEventListener("mouseout", function() {
+    if (!scoTTouch) {
+      scoTTouch = true;
+    }
+    res(scoTTouch, scoText, scoText, [equipIntr, checkText]);
+  });
+
+  function res(x, self, selfT, other) {
+    if (!x) {
+      self.style.width = "300px";
+      self.style.borderRadius = "150px";
+      selfT.style.display = "none";
+      other[0].style.display = "flex";
+      other[1].style.display = "flex";
+    }
+  }
+
+  /*equipIntr.addEventListener("mouseover", function() {
     equipIntr.style.width = "150px";
     equipIntr.style.borderRadius = "0";
     equipText.style.display = "flex";
     scoIntr.style.display = "none";
     checkIntr.style.display = "none";
-  });
-
-  equipIntr.addEventListener("mouseout", function() {
-    equipIntr.style.width = "300px";
-    equipIntr.style.borderRadius = "150px";
-    equipText.style.display = "none";
-    scoIntr.style.display = "flex";
-    checkIntr.style.display = "flex";
+    equipTTouch = true;
   });
 
   checkIntr.addEventListener("mouseover", function() {
@@ -116,14 +129,38 @@ window.onload = function() {
     checkText.style.display = "flex";
     equipIntr.style.display = "none";
     scoIntr.style.display = "none";
+    checkTTouch = true;
   });
 
-  checkIntr.addEventListener("mouseout", function() {
-    checkIntr.style.width = "300px";
-    checkIntr.style.borderRadius = "150px";
-    checkText.style.display = "none";
-    equipIntr.style.display = "flex";
-    scoIntr.style.display = "flex";
-  });
+  otherIntro.addEventListener("mouseout", function() {
+    if (scoTTouch) {
+
+
+      scoIntr.style.width = "300px";
+      scoIntr.style.borderRadius = "150px";
+      scoText.style.display = "none";
+      equipIntr.style.display = "flex";
+      checkIntr.style.display = "flex";
+      scoTTouch = false;
+    }
+    if (equipTTouch) {
+      equipIntr.style.width = "300px";
+      equipIntr.style.borderRadius = "150px";
+      equipText.style.display = "none";
+      scoIntr.style.display = "flex";
+      checkIntr.style.display = "flex";
+      equipTTouch = false;
+    }
+    if (checkTTouch) {
+      checkIntr.style.width = "300px";
+      checkIntr.style.borderRadius = "150px";
+      checkText.style.display = "none";
+      equipIntr.style.display = "flex";
+      scoIntr.style.display = "flex";
+      checkTTouch = false;
+    }
+  });*/
+
+
 
 }
