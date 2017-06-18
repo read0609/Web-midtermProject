ed2f758732aecf3b14c6eee9fad684439d5996d2
DESKTOP-22A0EPS\user <mdyu1000@gmail.com>
2017-06-18 13:43:10 +0800

    關於赤弦 gulp OK

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var isClick__footer__share = false;
-var arrow = false;
+var arrow1 = false;
+var arrow2 = false;
+var arrow3 = false;
+var arrow4 = false;
 
 $('.footer__share').click(function() {
   if (!isClick__footer__share) {
@@ -12,71 +15,71 @@ $('.footer__share').click(function() {
 });
 
 $('.index__arr__down1').click(function() {
-  if (!arrow) {
+  if (!arrow1) {
     $('.index__about__square1__content1').addClass('content_display');
     $('.index__arr__down1').addClass('index__arr__down--hide');
     $('.index__arr__up1').addClass('index__arr__down--display');
-    arrow = true;
+    arrow1 = true;
   }
 });
 
 $('.index__arr__up1').click(function() {
-  if (arrow) {
+  if (arrow1) {
     $('.index__about__square1__content1').removeClass('content_display');
     $('.index__arr__down1').removeClass('index__arr__down--hide');
     $('.index__arr__up1').removeClass('index__arr__down--display');
-    arrow = false;
+    arrow1 = false;
   }
 });
 $('.index__arr__down2').click(function() {
-  if (!arrow) {
+  if (!arrow2) {
     $('.index__about__square2__content2').addClass('content_display');
     $('.index__arr__down2').addClass('index__arr__down--hide');
     $('.index__arr__up2').addClass('index__arr__down--display');
-    arrow = true;
+    arrow2 = true;
   }
 });
 
 $('.index__arr__up2').click(function() {
-  if (arrow) {
+  if (arrow2) {
     $('.index__about__square2__content2').removeClass('content_display');
     $('.index__arr__down2').removeClass('index__arr__down--hide');
     $('.index__arr__up2').removeClass('index__arr__down--display');
-    arrow = false;
+    arrow2 = false;
   }
 });
 $('.index__arr__down3').click(function() {
-  if (!arrow) {
+  if (!arrow3) {
     $('.index__about__square3__content3').addClass('content_display');
     $('.index__arr__down3').addClass('index__arr__down--hide');
     $('.index__arr__up3').addClass('index__arr__down--display');
-    arrow = true;
+    arrow3 = true;
   }
 });
 
 $('.index__arr__up3').click(function() {
-  if (arrow) {
+  if (arrow3) {
     $('.index__about__square3__content3').removeClass('content_display');
     $('.index__arr__down3').removeClass('index__arr__down--hide');
     $('.index__arr__up3').removeClass('index__arr__down--display');
-    arrow = false;
+    arrow3 = false;
   }
 });
 $('.index__arr__down4').click(function() {
-  if (!arrow) {
+  if (!arrow4) {
     $('.index__about__square4__content4').addClass('content_display');
     $('.index__arr__down4').addClass('index__arr__down--hide');
     $('.index__arr__up4').addClass('index__arr__down--display');
-    arrow = true;
+    arrow4 = true;
   }
 });
 
-$('.index__about__square4__content4').click(function() {
-  if (arrow) {
-    $('.content4').removeClass('content_display');
+$('.index__arr__up4').click(function() {
+  if (arrow4) {
+    $('.index__about__square4__content4').removeClass('content_display');
     $('.index__arr__down4').removeClass('index__arr__down--hide');
     $('.index__arr__up4').removeClass('index__arr__down--display');
-    arrow = false;
+    arrow4 = false;
   }
 });
 
